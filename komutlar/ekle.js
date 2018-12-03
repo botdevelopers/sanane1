258234a3b612a2a9d1bc0472dc7305ae0e05297b
botdevelopers <44641774+botdevelopers@users.noreply.github.com>
2018-12-03 20:47:44 +0300

    Update ekle.js

--- komutlar/ekle.js
+++ komutlar/ekle.js
@@ -3,23 +3,23 @@ const Discord = require('discord.js');
 
 exports.run = function(client, message, args) {
 
-	var öneri = args.slice(0).join(' ');
-	var guildID = "459698010473889813";
-	var channelID = "484779276114067467";
+	var botid = args.slice(0).join(' ');
+	var guildID = "518322924080463872";
+	var channelID = "519207531785224222";
 	
-	if (!öneri){
-		return message.reply("Bir mesaj belirtin! Doğru kullanım: **?öneri <mesaj>**");
+	if (!botid){
+		return message.reply("Bir ID Belirt! Doğru kullanım: **tr!ekle <Bot_ID>**");
 	} else {
 		
 		var embed = new Discord.RichEmbed()
 			.setTimestamp()
-			.addField("Eylem:", "Öneri")
-			.addField("Kullanıcı:", message.author.tag)
-			.addField("ID", message.author.id)
-			.addField("Öneri", öneri)
+			.addField("Eylem:", "Bot Ekleme İşlemi")
+			.addField("Bot Sahibi:", message.author.tag)
+			.addField("Bot Sahibi ID", message.author.id)
+			.addField("Bot ID", botid)
 		
 		client.guilds.get(guildID).channels.get(channelID).send(embed);
-		message.channel.send("Öneriniz alınmıştır! Teşekkür ederiz.");
+		message.channel.send("Botunuz Kuyruğa Alınmıştır.");
 	};
 
 
